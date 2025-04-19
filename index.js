c9cd5171b7c0f5734d0e7c6202ec1f3f7e29effb
Libx09 <ibrahimfuad729@gmail.com>
2025-04-20 01:23:28 +0600

    make agent req api fix few things

--- index.js
+++ index.js
@@ -14,17 +14,6 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 
-const verifyToken = (req, res, next) => {
-  const token = req.cookies?.token;
-  // console.log("token added before", token);
-  if (!token) return res.status(401).send("Access Denied");
-  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
-    if (err) return res.status(403).send("Invalid Token");
-    req.user = decoded;
-    next();
-  });
-};
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.iciu9bb.mongodb.net/MFS?appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -44,8 +33,9 @@ async function run() {
   try {
     // const usersCollection = client.db("MFS").collection("users");
 
-    app.post("/jwt", (req, res) => {
+    app.post("/jwt", async (req, res) => {
       const user = req.body;
+      console.log("38 user", user);
       const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
         expiresIn: "5h",
       });
@@ -56,6 +46,17 @@ async function run() {
         })
         .send({ success: true });
     });
+
+    const verifyToken = (req, res, next) => {
+      const token = req.cookies?.token;
+      console.log("token added before", token);
+      if (!token) return res.status(401).send("Access Denied");
+      jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err) return res.status(403).send("Invalid Token");
+        req.user = decoded;
+        next();
+      });
+    };
 
     app.post("/logout", (req, res) => {
       res
@@ -77,6 +78,42 @@ async function run() {
       const user = new User(req.body);
       const result = await user.save();
       res.send(result);
+    });
+
+    //Get Agents request
+    app.get("/users/agent-requests", async (req, res) => {
+      try {
+        const agentRequests = await User.find({
+          role: "user",
+          requestAgent: true,
+        });
+        res.send(agentRequests);
+      } catch (error) {
+        res
+          .status(500)
+          .send({ message: "Error fetching agent requests", error });
+      }
+    });
+
+    // checks roles
+    app.get("/users/roles/:email", async (req, res) => {
+      try {
+        const email = req.params.email;
+        const user = await User.findOne({ email });
+        console.log("88 user", user);
+
+        if (!user) {
+          return res.status(404).send({ message: "User Not Found" });
+        }
+
+        res.send({
+          role: user.role, // e.g., "user", "agent", or "admin"
+          isAdmin: user.role === "admin",
+          isAgent: user.role === "agent",
+        });
+      } catch (error) {
+        res.status(500).send({ message: "Server error", error });
+      }
     });
 
     // Connect the client to the server	(optional starting in v4.7)
